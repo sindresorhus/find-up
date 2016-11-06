28579ae9d0a663b994aff8f996be1222e08b73bf
Mathieu M-Gosselin <mathieumg@gmail.com>
2016-11-06 15:30:27 -0500

    Basic multi-file search implementation. (Refs #11)

--- index.js
+++ index.js
@@ -2,18 +2,44 @@
 const path = require('path');
 const pathExists = require('path-exists');
 
+const verifyIfPathFound = paths => new Promise(resolve => {
+	const nextPath = paths.next();
+
+	if (nextPath.done) {
+		return resolve();
+	}
+
+	return resolve(nextPath.value[1].then(exists => {
+		if (exists) {
+			return nextPath.value[0];
+		}
+
+		return verifyIfPathFound(paths);
+	}));
+});
+
+const searchPath = (dir, files) => files.map(file => {
+	const fp = path.join(dir, file);
+
+	return [fp, pathExists(fp)];
+});
+
+const findPath = (dir, files) => verifyIfPathFound(
+	searchPath(dir, files)[Symbol.iterator]()
+);
+
 module.exports = (filename, opts) => {
 	opts = opts || {};
 
 	const startDir = path.resolve(opts.cwd || '');
 	const root = path.parse(startDir).root;
 
+	const filenames = [].concat(filename);
+
 	return new Promise(resolve => {
 		(function find(dir) {
-			const fp = path.join(dir, filename);
-
-			pathExists(fp).then(exists => {
-				if (exists) {
+			findPath(dir, filenames).then(fp => {
+				if (fp) {
 					resolve(fp);
 				} else if (dir === root) {
 					resolve(null);
@@ -31,11 +57,17 @@ module.exports.sync = (filename, opts) => {
 	let dir = path.resolve(opts.cwd || '');
 	const root = path.parse(dir).root;
 
+	const filenames = [].concat(filename);
+
+	const findPathSync = files => files
+		.map(name => path.join(dir, name))
+		.find(filePath => pathExists.sync(filePath));
+
 	// eslint-disable-next-line no-constant-condition
 	while (true) {
-		const fp = path.join(dir, filename);
+		const fp = findPathSync(filenames);
 
-		if (pathExists.sync(fp)) {
+		if (fp) {
 			return fp;
 		} else if (dir === root) {
 			return null;

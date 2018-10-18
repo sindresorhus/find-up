f02346ad8da7b48925949d44365d2edfea2d9b60
Louis Orleans <louis@orleans.io>
2018-10-18 15:11:51 -0700

    ✨ add synchronous function def

--- index.d.ts
+++ index.d.ts
@@ -5,11 +5,32 @@ export interface IOptions {
 
 /**
  * Returns a `Promise` for either the filepath or `null` if it couldn't be found.
+ *
  * @param filename Filename of the file to find.
  */
-export default function (filename: string, opts?: IOptions): Promise<string | null>;
+declare function findUp (filename: string, options?: IOptions):
+	Promise<string | null>;
 /**
  * Returns a `Promise` for either the first filepath found (by respecting the order) or `null` if none could be found.
+ *
  * @param filename Filename of the file to find.
  */
-export default function (filename: string[], opts?: IOptions): Promise<string | null>;
+declare function findUp (filename: string[], options?: IOptions):
+	Promise<string | null>;
+
+declare namespace findUp {
+	/**
+	 * Returns a filepath or null.
+	 *
+	 * @param filename Filename of the file to find.
+	 */
+	function sync (filename: string, options?: IOptions): string | null;
+	/**
+	 * Returns the first filepath found (by respecting the order) or null.
+	 *
+	 * @param filename Filename of the file to find.
+	 */
+	function sync (filename: string[], options?: IOptions): string | null;
+}
+
+export default findUp;

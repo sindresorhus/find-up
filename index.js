80fda88e0f005dcfef11ee2cf87c6cf27f6893de
Sindre Sorhus <sindresorhus@gmail.com>
2019-04-18 17:55:12 +0700

    Require Node.js 8

--- index.js
+++ index.js
@@ -2,31 +2,31 @@
 const path = require('path');
 const locatePath = require('locate-path');
 
-module.exports = (name, options = {}) => {
-	const startDirectory = path.resolve(options.cwd || '');
-	const {root} = path.parse(startDirectory);
-
+module.exports = async (name, options = {}) => {
+	let directory = path.resolve(options.cwd || '');
+	const {root} = path.parse(directory);
 	const paths = [].concat(name);
 
-	return new Promise(resolve => {
-		(function find(directory) {
-			locatePath(paths, {cwd: directory}).then(foundPath => {
-				if (foundPath) {
-					resolve(path.join(directory, foundPath));
-				} else if (directory === root) {
-					resolve();
-				} else {
-					find(path.dirname(directory));
-				}
-			});
-		})(startDirectory);
-	});
+	// eslint-disable-next-line no-constant-condition
+	while (true) {
+		// eslint-disable-next-line no-await-in-loop
+		const foundPath = await locatePath(paths, {cwd: directory});
+
+		if (foundPath) {
+			return path.join(directory, foundPath);
+		}
+
+		if (directory === root) {
+			return;
+		}
+
+		directory = path.dirname(directory);
+	}
 };
 
 module.exports.sync = (name, options = {}) => {
 	let directory = path.resolve(options.cwd || '');
 	const {root} = path.parse(directory);
-
 	const paths = [].concat(name);
 
 	// eslint-disable-next-line no-constant-condition

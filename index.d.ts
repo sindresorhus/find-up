da1b331f95f503fb12057a4b040586c1f0bda77c
Sindre Sorhus <sindresorhus@gmail.com>
2019-05-07 11:31:02 +0700

    Various TypeScript type tweaks

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,11 @@
-import * as locatePath from 'locate-path';
+import {Options as LocatePathOptions} from 'locate-path';
+
+declare const stop: unique symbol;
 
 declare namespace findUp {
-	interface Options {
+	interface Options extends LocatePathOptions {}
+
+	interface MatcherOptions {
 		/**
 		Directory to start from.
 
@@ -10,7 +14,9 @@ declare namespace findUp {
 		readonly cwd?: string;
 	}
 
-	type Match = string | symbol | undefined;
+	type StopSymbol = typeof stop;
+
+	type Match = string | StopSymbol | undefined;
 }
 
 declare const findUp: {
@@ -43,7 +49,7 @@ declare const findUp: {
 	})();
 	```
 	*/
-	(name: string | string[], options?: locatePath.AsyncOptions): Promise<string | undefined>;
+	(name: string | string[], options?: findUp.Options): Promise<string | undefined>;
 
 	/**
 	Find a file or directory by walking up parent directories.
@@ -51,7 +57,7 @@ declare const findUp: {
 	@param matcher - Called for each directory in the search. Return a path or `findUp.stop` to stop the search.
 	@returns The first path found or `undefined` if none could be found.
 	*/
-	(matcher: (directory: string) => (findUp.Match | Promise<findUp.Match>), options?: findUp.Options): Promise<string | undefined>;
+	(matcher: (directory: string) => (findUp.Match | Promise<findUp.Match>), options?: findUp.MatcherOptions): Promise<string | undefined>;
 
 	/**
 	Synchronously find a file or directory by walking up parent directories.
@@ -59,7 +65,7 @@ declare const findUp: {
 	@param name - Name of the file or directory to find. Can be multiple.
 	@returns The first path found (by respecting the order of `name`s) or `undefined` if none could be found.
 	*/
-	sync(name: string | string[], options?: locatePath.Options): string | undefined;
+	sync(name: string | string[], options?: findUp.Options): string | undefined;
 
 	/**
 	Synchronously find a file or directory by walking up parent directories.
@@ -67,12 +73,12 @@ declare const findUp: {
 	@param matcher - Called for each directory in the search. Return a path or `findUp.stop` to stop the search.
 	@returns The first path found or `undefined` if none could be found.
 	*/
-	sync(matcher: (directory: string) => findUp.Match, options?: findUp.Options): string | undefined;
+	sync(matcher: (directory: string) => findUp.Match, options?: findUp.MatcherOptions): string | undefined;
 
 	/**
 	Return this in a `matcher` function to stop the search and force `findUp` to immediately return `undefined`.
 	*/
-	readonly stop: unique symbol;
+	readonly stop: findUp.StopSymbol;
 };
 
 export = findUp;
--- index.js
+++ index.js
@@ -8,10 +8,13 @@ module.exports = async (name, options = {}) => {
 	let directory = path.resolve(options.cwd || '');
 	const {root} = path.parse(directory);
 	const paths = [].concat(name);
+
 	// eslint-disable-next-line no-constant-condition
 	while (true) {
 		// eslint-disable-next-line no-await-in-loop
-		const foundPath = await (typeof name === 'function' ? name(directory) : locatePath(paths, {...options, cwd: directory}));
+		const foundPath = await (typeof name === 'function' ?
+			name(directory) :
+			locatePath(paths, {...options, cwd: directory}));
 
 		if (foundPath === stop) {
 			return;
@@ -36,7 +39,9 @@ module.exports.sync = (name, options = {}) => {
 
 	// eslint-disable-next-line no-constant-condition
 	while (true) {
-		const foundPath = typeof name === 'function' ? name(directory) : locatePath.sync(paths, {...options, cwd: directory});
+		const foundPath = typeof name === 'function' ?
+			name(directory) :
+			locatePath.sync(paths, {...options, cwd: directory});
 
 		if (foundPath === stop) {
 			return;
